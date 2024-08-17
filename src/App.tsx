0ea7eb76d13b443993aaaf1d5d59378e2f0a89ae
caleb <kalejaiyecaleb@gmail.com>
2024-08-17 23:01:10 +0100

    commit

---
+++ src/App.tsx
@@ -0,0 +1,21 @@
+import Nav from "../src/sections/Nav";
+import Hero from "../src/sections/Hero";
+import Features from "../src/sections/Features";
+import Partners from "../src/sections/Partners";
+import Reviews from "./sections/Reviews";
+import Footer from "./sections/Footer";
+
+const App = () => {
+  return (
+    <>
+    <Nav />
+    <Hero />
+    <Features />
+    <Partners />
+    <Reviews />
+    <Footer />
+    </>
+  )
+}
+
+export default App;
---
+++ src/common/ButtonLink.tsx
@@ -0,0 +1,17 @@
+export interface ButtonLinkProps {
+    href: string;
+    children?: React.ReactNode;
+    className?: string;
+    target?: string;
+    logo?: string;
+    upperText?: string;
+    lowerText: string;
+}
+
+const ButtonLink = ({ href, children, className, target }: ButtonLinkProps) => {
+  return (
+    <a href={href} children={children} className={className} target={target}></a>
+  )
+}
+
+export default ButtonLink;
